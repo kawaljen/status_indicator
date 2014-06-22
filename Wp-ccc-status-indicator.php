bad90d8d15d326fbf802a984a7058f90b7a1ac2b
kawaljen <declicsweb@gmail.com>
2014-06-22 12:14:41 +0100

    add upload image efature

--- Wp-ccc-status-indicator.php
+++ Wp-ccc-status-indicator.php
@@ -13,7 +13,7 @@ class ccc_si_widget extends WP_Widget {
 	function __construct() {
         
       
-        // css
+        // css and js
         add_action('wp_enqueue_scripts', array(&$this, 'ccc_si_register_scripts'));
         add_action('admin_enqueue_scripts', array(&$this, 'ccc_si_admin_scripts'));
 		
@@ -25,75 +25,93 @@ class ccc_si_widget extends WP_Widget {
     function ccc_si_admin_scripts($hook) {
         if ($hook != 'widgets.php')
             return;
-        wp_register_script('ccc_si_widget_admin', plugins_url('js/cccsi-admin.js', __FILE__), array('jquery'));  
-        wp_enqueue_script('ccc_si_widget_admin');
+	    wp_enqueue_style('thickbox');        
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('media-upload');
+		wp_enqueue_script('thickbox');
+	    wp_register_script('cccsi_widget_admin', plugins_url('js/cccsi-admin.js', __FILE__), array('jquery', 'media-upload','thickbox'));  
+		wp_enqueue_script('cccsi_widget_admin');
     }
     function ccc_si_register_scripts() { 
 		// JS    
-		wp_register_script('ccc_si_widget', plugins_url('js/cccsi-widget.js', __FILE__), array('jquery'));     
-		wp_localize_script( 'ccc_si_widget', 'oxford',         
+		wp_register_script('cccsi_widget', plugins_url('js/cccsi-widget.js', __FILE__), array('jquery'));     
+		wp_localize_script( 'cccsi_widget', 'cccsi',         
 			array( 'ajax_url' => admin_url( 'admin-ajax.php' )) 
 		);        
 		// CSS     
-		wp_register_style('ccc_si', plugins_url('css/cccsi-widget.css', __FILE__), true);
+		wp_register_style('cccsi_widget', plugins_url('css/cccsi-widget.css', __FILE__), true);
     }  
     	
 	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 	'notrunning' => array('id' => 1, 'img' => 'not-running.png'), 
-																'disable' => array('id' => 2, 'img' => 'disable.png'), 
-																'running' => array('id' => 3, 'img' => 'running.png') ));
+		$instance = wp_parse_args( (array) $instance, array( 	'down' => array('id' => 1, 'img' => 'not-up.png'), 
+																'unknown' => array('id' => 2, 'img' => 'disable.png'),
+																'up' => array('id' => 3, 'img' => 'up.png'),
+																'size' => 100 ));
 		extract($instance);
 		?>
         <div class="ccc_si">
         
-        <h4>Images</h4>
-        
+        <h4>Status images</h4>    
 		<div>
-
-			<div >
-				<label  for="<?php echo $this->get_field_name("notrunning"); ?>[img]">Not running : </label>
-				<input type="text" class="text" id="<?php echo $this->get_field_id("notrunning"); ?>[img]" name="<?php echo $this->get_field_name("notrunning"); ?>[img]" size="12" value="<?php if (isset($notrunning['img'])) { echo $notrunning['img'];} ?>" />				
+			<p>Enter an image url or use the media uploader and click on 'Insert into post'</p>
+			<div>
+				<label  for="<?php echo $this->get_field_name("down"); ?>[img]">Not up : </label>
+				<input id="<?php echo $this->get_field_id("down"); ?>[img]" type="text" name="<?php echo $this->get_field_name("down"); ?>[img]" value="<?php if (isset($down['img'])) { echo $down['img'];} ?>" size="35" />
+				<input  class="cccsi-upload-button button" type="button" value="Upload Image" />    
 			</div>
 
-			<div >
-				<label  for="<?php echo $this->get_field_name("disable"); ?>[img]">Disable : </label>
-				<input type="text" class="text" id="<?php echo $this->get_field_id("disable"); ?>[img]" name="<?php echo $this->get_field_name("disable"); ?>[img]" size="12" value="<?php if (isset($disable['img'])) { echo $disable['img'];} ?>" />				
+			<div style="margin-top:10px;">
+				<label  for="<?php echo $this->get_field_name("unknown"); ?>[img]">unknown : </label>
+				<input id="<?php echo $this->get_field_id("unknown"); ?>[img]" type="text" name="<?php echo $this->get_field_name("unknown"); ?>[img]" value="<?php if (isset($unknown['img'])) { echo $unknown['img'];} ?>" size="35" />
+				<input  class="cccsi-upload-button button" type="button" value="Upload Image" />    
 			</div>
 
-			<div >
-				<label  for="<?php echo $this->get_field_name("running"); ?>[img]">Running : </label>
-				<input type="text" class="text" id="<?php echo $this->get_field_id("running"); ?>[img]" name="<?php echo $this->get_field_name("running"); ?>[img]" size="12" value="<?php if (isset($running['img'])) { echo $running['img'];} ?>" />				
+			<div style="margin-top:10px;">
+				<label  for="<?php echo $this->get_field_name("up"); ?>[img]">up : </label>
+				<input id="<?php echo $this->get_field_id("up"); ?>[img]" type="text" name="<?php echo $this->get_field_name("up"); ?>[img]" value="<?php if (isset($up['img'])) { echo $up['img'];} ?>" size="35" />
+				<input  class="cccsi-upload-button button" type="button" value="Upload Image" /> 	
 			</div>			  
 		</div>
-        <div class="clear"></div>
+         <h4>Images size</h4> 
+				<label  for="<?php echo $this->get_field_name("size"); ?>">Size in px : </label>
+				<input id="<?php echo $this->get_field_id('size'); ?>_" name="<?php echo $this->get_field_name('size'); ?>" type="number" min="10" step="10" value="<?php echo $size; ?>" />	
 
-
-		</div><!--end oxford thing  -->
+		</div><!--end cccsi -->
 		<?php 
 	}		
 	
 	function update( $new_instance, $old_instance ) {	
 		$instance = $old_instance;    
-		$instance['notrunning'] = $new_instance['notrunning'];
-		$instance['disable'] = $new_instance['disable'];
-		$instance['running'] = $new_instance['running'];
+		$instance['down'] = $new_instance['down'];
+		$instance['unknown'] = $new_instance['unknown'];
+		$instance['up'] = $new_instance['up'];
+		$instance['size'] = $new_instance['size'];
 		return $instance;	
 	}	
 	function widget( $args, $instance ) {	
-		extract($args);     
+		extract($args);       
 		extract($instance);    
-		wp_enqueue_script('ccc_si_widget'); 
-		wp_enqueue_style('ccc_si_widget');  
+		wp_enqueue_script('cccsi_widget'); 
+		wp_enqueue_style('cccsi_widget');  
         
 		?>	
-		<div class="ccc_si_content">		
-				<!-- add front end content here -->
-		</div>   
-		<script type="text/javascript">  
-			jQuery(function($) {    
-				$('#<?php echo $widget_id; ?>_content').data('args', <?php echo json_encode($instance); ?>);  
-			});  
-		</script>  
+		<?php echo $before_widget; ?>
+		
+		<div class="ccc_si_content" id="<?php echo $widget_id;?>">
+		<script type="text/javascript">
+			jQuery(function($) {
+                widget_config = {
+                     'image_id':'status_indicator',
+                     'states':  <?php echo json_encode($instance); ?>  ,
+                     'status_endpoint':'http://understanding-geek.com/status_indicator/status.php'
+                }
+				$('#<?php echo $widget_id; ?>').data('args', widget_config);
+
+			});
+		</script>
+		</div>
+		
+		<?php echo $after_widget; ?>
 <?php 	
 	}  
 	
@@ -102,7 +120,4 @@ class ccc_si_widget extends WP_Widget {
 }
 	
 add_action( 'widgets_init', create_function( '', 'register_widget( "ccc_si_widget" );' ) );
-//add_action( 'widgets_init', function(){ register_widget( 't2A_widget' );}); +++ PHP 5.3+ only
-
-
 ?>
